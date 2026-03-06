21757e7571c88375cb72227ab3ddc5a3cd4c9408
NicolasR <nicolas.romanoff94@gmail.com>
2026-03-06 12:01:26 +0100

    fix: node-ip

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,4 +1,4 @@
-curl -sfL https://get.k3s.io | sh -s -
+curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
--- p1/scripts/serverWorker.sh
+++ p1/scripts/serverWorker.sh
@@ -1,3 +1,3 @@
 TOKEN=$(sudo cat /vagrant/token)
 
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh - --node-ip 192.168.56.111
