4d70c66f334f6547e789157f897b282770d6bc36
NicolasR <nicolas.romanoff94@gmail.com>
2026-03-07 16:27:20 +0100

    refacto: rm useless kubectl install

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,5 +1,3 @@
 curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,7 +1,5 @@
 curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
 sudo kubectl apply -f /vagrant/confs/app-one.yaml
 sudo kubectl apply -f /vagrant/confs/app-two.yaml
 sudo kubectl apply -f /vagrant/confs/app-three.yaml
