5b3eec46233008d35ee4ad3d81ba3ef2b6023d93
NicolasR <nicolas.romanoff94@gmail.com>
2026-03-10 14:06:16 +0100

    feat: bonus vagrant

--- bonus/scripts/server.sh
+++ bonus/scripts/server.sh
@@ -1,11 +1,11 @@
 sudo apt update
 sudo apt install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
-URIs: https://download.docker.com/linux/debian
+URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
@@ -18,7 +18,7 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-sudo k3d cluster create mycluster
+sudo k3d cluster create mycluster -p "80:80@loadbalancer"
 
 sudo kubectl create namespace dev
 sudo kubectl create namespace argocd
@@ -28,7 +28,12 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
-sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+sudo kubectl patch configmap argocd-cmd-params-cm -n argocd --type merge -p '{"data":{"server.insecure":"true"}}'
+sudo kubectl rollout restart deployment argocd-server -n argocd
+sudo kubectl apply -f /vagrant/confs/ingress.yaml
+
+# For get the password of argocd with "admin" username
+# argocd admin initial-password -n argocd
 
 sudo kubectl create namespace gitlab
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
